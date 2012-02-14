074265d621b78791ed837a6e585bebdff04d71de
Yusuke Mito <y310.1984@gmail.com>
2012-02-15 02:32:46 +0900

    render :partial in callback causes following error because view_paths expects ActionView::OptimizedFileSystemResolver from rails 3.2.0
    undefined method `find_all' for "/Users/alice/work/project/app/chankos/foo/views":String

--- spec/integration/chanko_spec.rb
+++ spec/integration/chanko_spec.rb
@@ -5,14 +5,14 @@ describe 'Invoker', :type => :integration do
     ext_mock("AcceptanceTest")
     AcceptanceTest.class_eval do
       scope(:view) do
-        callback(:render) { 'render view' }
+        callback(:render) { render :partial => '/show' }
       end
     end
   end
 
   it 'invoke with view' do
     visit '/invoke/with_view'
-    page.should have_content('render view')
+    page.should have_content('render view file')
     page.should have_selector('div', :class => %w(extension ext_acceptance_test ext_acceptance_test-render))
   end
 end
