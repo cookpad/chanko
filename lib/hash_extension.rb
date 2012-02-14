a4340eb3d73dc5684780b55e5b5b7e8394f6fa93
Akira Matsuda <ronnie@dio.jp>
2012-02-14 18:33:22 +0900

    gomi

--- lib/hash_extension.rb
+++
@@ -1,9 +0,0 @@
-Hash.class_eval do
-  def self.default_is_hash(number=1)
-    if number > 0
-      self.new { |h,v| h[v] = default_is_hash(number - 1 ) }
-    else
-      self.new
-    end
-  end
-end
