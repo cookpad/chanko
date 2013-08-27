b10827b6b2724884b74acead549be2df18fe64c3
Fuji, Goro <goro-fuji@cookpad.com>
2013-08-27 17:06:29 +0900

    no \b nor /x in regular expressions for maintainability

--- spec/chanko/deprecated_spec.rb
+++ spec/chanko/deprecated_spec.rb
@@ -14,15 +14,15 @@ module Chanko
 
       it "uses options in #new" do
         _ = ActiveIf.new foo: 'bar'
-        expect($stderr.string).to match(/\b deprecated \b/xms)
-        expect($stderr.string).to match(/#{Regexp.escape __FILE__}/xms)
+        expect($stderr.string).to match(/ deprecated /)
+        expect($stderr.string).to match(/#{Regexp.escape __FILE__}/)
       end
 
       it "uses #options" do
         o = ActiveIf.new
         expect(o.options).to be_empty
-        expect($stderr.string).to match(/\b deprecated \b/xms)
-        expect($stderr.string).to match(/#{Regexp.escape __FILE__}/xms)
+        expect($stderr.string).to match(/ deprecated /)
+        expect($stderr.string).to match(/#{Regexp.escape __FILE__}/)
       end
     end
   end
